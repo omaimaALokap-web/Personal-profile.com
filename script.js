8574b6864a6e1b50cf3b265cb3400255aa8b5689
omaimaALokap-web <66798606+omaimaALokap-web@users.noreply.github.com>
2022-06-08 13:25:39 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,39 @@
+console.log("it is working")
+
+let theme = localStorage.getItem('theme')
+
+if(theme == null){
+    setTheme('light')
+}else{
+    setTheme(theme)
+}
+console.log(theme)
+
+let themedot=document.getElementsByClassName('theme-dont')
+
+for( var i=0; themedot.length>i;i++){
+    themedot[i].addEventListener('click',function(){
+        let mode = this.dataset.mode
+        console.log('option clicked',mode)
+        setTheme(mode)
+    })
+}
+
+function setTheme(mode){
+    if( mode == 'light'){
+        document.getElementById('theme-style').href = 'default.css'
+       
+    }
+    if( mode == 'blue'){
+        document.getElementById('theme-style').href = 'blue.css' 
+    }
+    if( mode == 'green'){
+        document.getElementById('theme-style').href = 'green.css' 
+    }
+    if( mode == 'purple'){
+        document.getElementById('theme-style').href = 'purple.css' 
+    }
+
+    localStorage.setItem('theme', mode)
+    
+}
